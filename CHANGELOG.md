5e371efcd91a820d11168f9d6ab7c45a51e1e4ce
petitphp <c.boirie@gmail.com>
2016-11-11 18:16:35 +0100

    update changelog and bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.1 (November 11, 2016)
+
+* Fix fatal error when the autoload file is not in the plugin directory
+
 ## 1.0.0 (October 8, 2016)
 
 * Initial release
--- searchwp-stemmer.php
+++ searchwp-stemmer.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: SearchWP Stemmer
 Description: Add additionnals stemmers to SearchWP.
-Version: 1.0.0
+Version: 1.0.1
 Author: BeAPI
 Author URI: https://beapi.fr
 Text Domain: searchwp-stemmer
@@ -36,7 +36,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'SWP_STEMMER_VERSION', '1.0.0' );
+define( 'SWP_STEMMER_VERSION', '1.0.1' );
 
 if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	/**
